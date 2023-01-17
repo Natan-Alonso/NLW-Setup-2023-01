d5b416418949da085c370347a3c2256d5b21342c
Natan Alonso <natan.oliveira2002@hotmail.com>
2023-01-17 16:42:45 -0300

    initial commit of the aula1

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+/**
+ * Metodo HTTP: Get, Post, Put, Patch, Delete
+ */
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+    
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running')
+})
