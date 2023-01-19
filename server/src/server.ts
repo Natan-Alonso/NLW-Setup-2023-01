d2fb8d043be66fe17f987bb5a5ed2960584d2caf
Natan Alonso <natan.oliveira2002@hotmail.com>
2023-01-19 20:32:28 -0300

    second commit aula1

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
 const prisma = new PrismaClient()
 
-/**
- * Metodo HTTP: Get, Post, Put, Patch, Delete
- */
+app.register(cors)
 
 app.get('/hello', async () => {
     const habits = await prisma.habit.findMany()
